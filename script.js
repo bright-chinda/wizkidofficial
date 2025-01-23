c5519546a36ba23183056f0b4cacfd179aa77572
bright-chinda <brightnwenenda81@gmail.com>
2025-01-23 00:56:11 -1200

    mm

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let dropDown = document.querySelector(".links")
 let menuBtn = document.querySelector(".menu-btn")
 let exitBtn = document.querySelector(".exit-btn")
+let tabRemove = document.querySelectorAll(".tab-list")
+
+tabRemove.forEach(element => {
+    element.addEventListener("click", ()=>{
+        dropDown.classList.remove("active")
+    })
+});
 
 menuBtn.addEventListener("click",function(){
     dropDown.classList.add("active")
